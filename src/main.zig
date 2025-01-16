b0d23de132c60748c59788d0209fe60d300e84cb
Ignacio Ibarra <iknacx@gmail.com>
2025-01-16 16:45:11 -0300

    drawing a quad

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,16 @@ const ShaderProgram = @import("ShaderProgram.zig");
 
 const log = std.log.scoped(.main);
 
+const vertices: []const f32 = &.{
+    //|  pos  |    uv   |
+    -0.5, -0.5, 0.0, 0.0,
+    0.5,  -0.5, 1.0, 0.0,
+    0.5,  0.5,  0.0, 1.0,
+    -0.5, 0.5,  1.0, 1.0,
+};
+
+const indices: []const u32 = &.{ 0, 1, 2, 3 };
+
 fn onResize(_: ?*c.GLFWwindow, w: c_int, h: c_int) callconv(.C) void {
     width = w;
     height = h;
@@ -55,7 +65,6 @@ pub fn main() !void {
 
     if (c.gladLoadGLLoader(@ptrCast(&c.glfwGetProcAddress)) == c.GL_FALSE) return error.glInit;
     c.glDebugMessageCallback(&onGLError, null);
-
     _ = c.glfwSetFramebufferSizeCallback(window, onResize);
 
     var sp = ShaderProgram.init();
@@ -65,34 +74,32 @@ pub fn main() !void {
     try sp.link();
     sp.use();
 
-    const vertices: []const f32 = &.{
-        -1.0, -1.0, 0.0, 1.0, 0.0, 0.0,
-        1.0,  -1.0, 0.0, 0.0, 1.0, 0.0,
-        0.0,  1.0,  0.0, 0.0, 0.0, 1.0,
-    };
-
     const attrib_pos = 0;
-    const attrib_col = 1;
-
-    var vbo: u32 = undefined;
-    c.glCreateBuffers(1, &vbo);
-
-    c.glNamedBufferStorage(vbo, vertices.len * @sizeOf(f32), @ptrCast(vertices), c.GL_DYNAMIC_STORAGE_BIT);
+    const attrib_uv = 1;
 
     var vao: u32 = undefined;
     c.glCreateVertexArrays(1, &vao);
 
+    var vbo: u32 = undefined;
+    c.glCreateBuffers(1, &vbo);
+    c.glNamedBufferStorage(vbo, vertices.len * @sizeOf(f32), @ptrCast(vertices), c.GL_DYNAMIC_STORAGE_BIT);
+
+    var ebo: u32 = undefined;
+    c.glCreateBuffers(1, &ebo);
+    c.glNamedBufferStorage(ebo, indices.len * @sizeOf(u32), @ptrCast(indices), c.GL_DYNAMIC_STORAGE_BIT);
+
     const vao_binding_point = 0;
-    c.glVertexArrayVertexBuffer(vao, vao_binding_point, vbo, 0, @sizeOf(f32) * 6);
+    c.glVertexArrayVertexBuffer(vao, vao_binding_point, vbo, 0, @sizeOf(f32) * 4);
+    c.glVertexArrayElementBuffer(vao, ebo);
 
     c.glEnableVertexArrayAttrib(vao, attrib_pos);
-    c.glEnableVertexArrayAttrib(vao, attrib_col);
+    c.glEnableVertexArrayAttrib(vao, attrib_uv);
 
-    c.glVertexArrayAttribFormat(vao, attrib_pos, 3, c.GL_FLOAT, c.GL_FALSE, 0);
-    c.glVertexArrayAttribFormat(vao, attrib_col, 3, c.GL_FLOAT, c.GL_FALSE, @sizeOf(f32) * 3);
+    c.glVertexArrayAttribFormat(vao, attrib_pos, 2, c.GL_FLOAT, c.GL_FALSE, 0);
+    c.glVertexArrayAttribFormat(vao, attrib_uv, 2, c.GL_FLOAT, c.GL_FALSE, @sizeOf(f32) * 2);
 
     c.glVertexArrayAttribBinding(vao, attrib_pos, vao_binding_point);
-    c.glVertexArrayAttribBinding(vao, attrib_col, vao_binding_point);
+    c.glVertexArrayAttribBinding(vao, attrib_uv, vao_binding_point);
 
     c.glViewport(0, 0, width, height);
 
@@ -101,7 +108,9 @@ pub fn main() !void {
         c.glClear(c.GL_COLOR_BUFFER_BIT);
 
         c.glBindVertexArray(vao);
-        c.glDrawArrays(c.GL_TRIANGLES, 0, 3);
+
+        c.glDrawElements(c.GL_TRIANGLE_FAN, 4, c.GL_UNSIGNED_INT, null);
+
         c.glfwSwapBuffers(window);
         c.glfwPollEvents();
     }
